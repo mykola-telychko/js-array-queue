ed1fb67870c6f462592acbd78beaf8aadfd68027
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2024-04-22 05:47:32 +0300

    Update index.js
    scnd version demo

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
-function addToQueue(arr, newNumber) {
-  // check new num // Перевіряємо, чи існує нове число в масиві
+let arr = [1, 2, 3];
+const limit = arr.length; // Ліміт довжини масиву
+
+function addToQueue(newNumber) {
+  // Перевіряємо, чи існує нове число в масиві
   if (!arr.includes(newNumber)) {
-    // add new unique // Додаємо нове унікальне число на початок масиву
-    arr.unshift(newNumber);
-    
-    // remove last // Якщо довжина масиву перевищує ліміт, видаляємо останній елемент
-    if (arr.length > limit) {
-      arr.pop();
+    // Зміщуємо кожний елемент масиву вправо на одну позицію
+    for (let i = arr.length - 1; i > 0; i--) {
+      arr[i] = arr[i - 1];
     }
+    
+    // Додаємо нове унікальне число на початок масиву
+    arr[0] = newNumber;
   }
   
-  return arr; 
+  // Якщо довжина масиву перевищує ліміт, видаляємо останній елемент
+  if (arr.length > limit) {
+    arr.pop();
+  }
 }
 
-let arr = [1, 2, 3];
-const limit = 3; // length arr
 
-// add new 
-arr = addToQueue(arr, 4);
+addToQueue(14);
 console.log(arr); // [4, 1, 2]
 
-// try duplicate // Спроба додати число 2 (вже існує в масиві)
-arr = addToQueue(arr, 2);
+addToQueue(22);
+console.log(arr); // [2, 4, 1]
+
+addToQueue(51);
+console.log(arr); // [5, 2, 4]
+
+addToQueue(4);
 console.log(arr); // [4, 1, 2]
 
-// add new 
-arr = addToQueue(arr, 5);
-console.log(arr); // [5, 4, 1]
+// Спроба додати число 2 (вже існує в масиві)
+addToQueue(2);
+console.log(arr); // [2, 4, 1]
+
+addToQueue(5);
+console.log(arr); // [5, 2, 4]
